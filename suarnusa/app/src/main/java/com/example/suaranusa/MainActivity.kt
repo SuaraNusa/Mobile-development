55a2d52087b7ea2ff6c795d801e6f15132494dbf
M. Naufal Faqih <naufalfaqih443@gmail.com>
2024-11-12 20:45:49 +0700

    partial update

--- suarnusa/app/src/main/java/com/example/suaranusa/MainActivity.kt
+++ suarnusa/app/src/main/java/com/example/suaranusa/MainActivity.kt
@@ -39,10 +39,11 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController.navigate(R.id.navigation_home)
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_blank
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
