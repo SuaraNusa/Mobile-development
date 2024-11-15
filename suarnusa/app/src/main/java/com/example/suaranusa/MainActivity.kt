afda28eac6cfda3211bef927799f31280e232e7e
M. Naufal Faqih <naufalfaqih443@gmail.com>
2024-11-15 19:51:48 +0700

    update

--- suarnusa/app/src/main/java/com/example/suaranusa/MainActivity.kt
+++ suarnusa/app/src/main/java/com/example/suaranusa/MainActivity.kt
@@ -34,6 +34,10 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
+        // set intent to auth
+        val intent = Intent(this, TabLayout::class.java)
+        startActivity(intent)
+        finish()
 
         val navView: BottomNavigationView = binding.navView
 
